0e0997259450ff09eab58304c2a97e5fa0400252
Денис <denisx121en64@gmail.com>
2022-05-26 20:03:33 +0400

    DTO

---
+++ src/main/java/com/Neoflex/conveyor/model/LoanOfferDTO.java
@@ -0,0 +1,23 @@
+package com.Neoflex.conveyor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanOfferDTO {
+
+    private Long applicationId;
+    private BigDecimal requestedAmount;
+    private BigDecimal totalAmount;
+    private Integer term;
+    private BigDecimal monthlyPayment;
+    private BigDecimal rate;
+    private boolean isInsuranceEnabled;
+    private boolean isSalaryClient;
+
+}
