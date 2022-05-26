bd064335d4486a2437f6022e10bc88cfe71f0f97
Денис <denisx121en64@gmail.com>
2022-05-26 19:30:39 +0400

    init

---
+++ src/test/java/com/Neoflex/conveyor/ConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Neoflex.conveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
